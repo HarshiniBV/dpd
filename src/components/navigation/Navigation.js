9498d30d6a6bb00857ad69463df43d6f85071a00
HarshiniBV <harshinibvh@gmail.com>
2024-08-09 08:20:16 +0530

    logo in nav

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload, faHandPaper, faHandRock, faHouseMedicalCircleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { faHandPaper, faHandRock, faHouseMedicalCircleExclamation } from '@fortawesome/free-solid-svg-icons';
 import './Navigation.css';
+import a from '../images/logo.jpg'
 
 function Navigation() {
   const username = localStorage.getItem('name');
 
   return (
     <div>
-      <ul className='nav bg-dark bg-gradient justify-content-between'>
-        <li className='title text-white pt-2'>DuplicaXpert</li>
+      <ul className='nav bg-dark bg-gradient justify-content-between p-2'>
+        <li className='title text-white'><img src={a} className='img11' /></li>
         <div className='d-flex'>
           <li className='nav-item'>
             <Link className='nav-link text-white' to=''>Home</Link>
